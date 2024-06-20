377789a90aa71a8ac257bbae5da7e05b1bcf7641
yushucs <yu.shucs@outlook.com>
2024-06-20 22:17:52 +0800

    SpringBoot项目初始化

---
+++ src/main/java/com/example/meogirlmusic/MeogirlMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.meogirlmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeogirlMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeogirlMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meogirlmusic/MeogirlMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meogirlmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeogirlMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
