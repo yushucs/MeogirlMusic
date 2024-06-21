ceb397b5ffc4ee68920a139422b2b6aaaab31bf7
yushucs <yu.shucs@outlook.com>
2024-06-21 09:04:41 +0800

    Update

--- src/main/java/com/example/meogirlmusic/MeogirlMusicApplication.java
+++ src/main/java/com/example/meogirlmusic/MeogirlMusicApplication.java
@@ -1,9 +1,12 @@
 package com.example.meogirlmusic;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.meogirlmusic.mapper")
 public class MeogirlMusicApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/meogirlmusic/mapper/UserMapper.java
+++ src/main/java/com/example/meogirlmusic/mapper/UserMapper.java
@@ -1,9 +1,7 @@
 package com.example.meogirlmusic.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 public interface UserMapper<User> extends BaseMapper<User> {
 
 }
