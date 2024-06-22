c2b3471a6193a398954a6ce14808848879f29c74
yushucs <yu.shucs@outlook.com>
2024-06-22 16:45:55 +0800

    update

--- src/main/java/com/example/meogirlmusic/service/SingerService.java
+++ src/main/java/com/example/meogirlmusic/service/SingerService.java
@@ -3,7 +3,6 @@ package com.example.meogirlmusic.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.meogirlmusic.entity.Singer;
 
-import java.util.List;
 
 public interface SingerService extends IService<Singer> {
 }
